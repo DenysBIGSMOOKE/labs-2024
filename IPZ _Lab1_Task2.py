0bb810fd4c72f3e9b144bc9695d693277bcf0874
DenysBIGSMOOKE <117598676+DenysBIGSMOOKE@users.noreply.github.com>
2024-09-13 19:20:27 +0300

    Add files via upload

---
+++ IPZ _Lab1_Task2.py
@@ -0,0 +1,14 @@
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+
+
+n = int(input("Введіть число: "))
+
+result = fibonacci(n)
+print(f"Число Фібоначчі для {n} = {result}")
